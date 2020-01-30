a168abe0a564cdfb0ec7bc43affdee11b0283a06
Santiago <santiagocampos04@gmail.com>
2020-01-30 21:40:45 +0100

    Adding Hash

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app =express();
 let config=require("./config.json");
-
-
-
-
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -29,7 +25,6 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User =new mongoose.model("User",userSchema);
 
@@ -37,7 +32,7 @@ app.post("/register",function(req,res){
 const newUser =new User({
   email: req.body.username
   ,
-  password:req.body.password
+  password:md5(req.body.password)
 });
 newUser.save(function(err){
   if(err){
@@ -51,7 +46,7 @@ newUser.save(function(err){
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
 
   User.findOne({email:username},function(err,foundUser){
     if (err){
